22808fe8c8503c69c987cc435531f9b725642065
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-08-13 11:07:57 +0100

    Public handler added
    Relates #20
    
    Co-authored-by: khadija-nur <59174800+khadija-nur@users.noreply.github.com>

--- src/handlers.js
+++ src/handlers.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const model = require("./model");
 
-//-------Home Handler------------------
+// -------Home Handler------------------
 function home(request, response) {
-  const filePath = path.join(__dirname, "index.html");
+  const filePath = path.join(__dirname, "..", "public", "index.html");
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.error(error);
@@ -18,9 +18,35 @@ function home(request, response) {
 }
 
 // --------Missing handler---------------
-function missing(response, request) {
-  response.writeHead(200, { "content-type": "text/html" });
-  response.end(`<h1>Oops, nothing for you over here</h1>`);
+function missing(request, response) {
+  response.writeHead(404, { "content-type": "text/html" });
+  response.end("<h1>Oops, nothing for you over here</h1>");
 }
 
-module.exports = { home, missing };
+// --------Public handler----------------
+const types = {
+  html: "text/html",
+  css: "text/css",
+  js: "application/javascript",
+};
+
+function public(request, response) {
+  const url = request.url;
+  const urlArray = url.split(".");
+  const extension = urlArray[urlArray.length - 1];
+  const type = types[extension];
+
+  const filePath = path.join(__dirname, "..", url);
+
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      response.writeHead(404, { "content-type": "text/html" });
+      response.end("<h1>Not found</h1>");
+    } else {
+      response.writeHead(200, { "content-type": type });
+      response.end(file);
+    }
+  });
+}
+
+module.exports = { home, missing, public };
