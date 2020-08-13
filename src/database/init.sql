b9cdac5536c04699aae7b544b7e6258d70043b8b
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-08-13 14:44:10 +0100

    Generated dummy data for testing
    Relates #6
    
    Co-authored-by: Josh <joshua.hart@protonmail.com>

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,20 +1,32 @@
 BEGIN;
 
-    CREATE TABLE users
-    (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(255) NOT NULL,
-        location VARCHAR(255)
-    );
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    location VARCHAR(255)
+);
 
-    CREATE TABLE posts
-    (
-        id SERIAL PRIMARY KEY,
-        user_id INTEGER REFERENCES users(id),
-        text_content TEXT NOT NULL
-    );
+CREATE TABLE posts
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    text_content TEXT NOT NULL
+);
+
+INSERT INTO users VALUES
+    (1, "Zi_You_in_Hell", "Behind you"),
+    (2, "jhart5", "A dark basement"),
+    (3, "Ephivecent", "Nowhere"),
+    (4, "Khadija", "The Matrix")
+;
 
 
+INSERT INTO posts VALUES
+    (1, 2, "Testing; one, two.")
+    (2, 4, "I love marmalade so much. It is the best.")
+    (3, 2, "Bill Gates is responsible for coronavirus.")
+    (4, 1, "Nailed it!!!")
+;
 
-
-    COMMIT;
+COMMIT;
---
+++ src/tests/model.test.js
@@ -0,0 +1,5 @@
+const test = require("tape");
+const build = require("../database/build.js");
+const { getUsers, createUser, getPosts, createPost } = require("../model");
+
+test("Can get users", t => {});
